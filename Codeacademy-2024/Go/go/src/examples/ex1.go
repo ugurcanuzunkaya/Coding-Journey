449df17c2fc44fe94ed5848fc075b9717216d016
mrrelaxed <ugurcanuzunkaya1@gmail.com>
2024-02-08 11:41:33 +0300

    v0.008
    added some examples related to the notes.ipynb

---
+++ Codeacademy-2024/Go/go/src/examples/ex1.go
@@ -0,0 +1,9 @@
+package main
+
+// Import the fmt package to use the Println function
+import "fmt"
+
+// Define the main function that will execute when you run the Go program
+func main() {
+	fmt.Println("Hello World")
+}
---
+++ Codeacademy-2024/Go/go/src/examples/ex3.go
@@ -0,0 +1,13 @@
+package main
+
+// Import the time package to use the time.Now() function
+import (
+  "fmt"
+  t "time"
+)
+
+// Define the main function that will execute when you run the Go program
+func main() {
+	fmt.Println(t.Now())
+}
+
