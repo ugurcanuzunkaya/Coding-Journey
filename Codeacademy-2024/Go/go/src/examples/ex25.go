547cceb06d0d5fe6c6d6d336074ba3eca8dc69e6
ugurcanuzunkaya <ugurcanuzunkaya1@gmail.com>
2024-03-12 02:46:12 +0300

    v0.0025
    learning go (if and else cases)

---
+++ Codeacademy-2024/Go/go/src/examples/ex25.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// heistReady is a boolean variable
+	heistReady := false
+
+	// if heistReady is true, print "Let's go!"
+	if heistReady {
+		fmt.Println("Let's go!")
+	} else {
+		// if heistReady is false, print "Act normal"
+		fmt.Println("Act normal")
+	}
+
+	// Output: Act normal
+
+}
