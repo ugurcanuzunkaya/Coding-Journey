5efc56c057eda460786895ba730035a0b4e08de6
Uğurcan Uzunkaya <ugurcanuzunkaya1@gmail.com>
2024-03-17 05:08:21 +0300

    v0.0029
    learning go (else if statement)

---
+++ Codeacademy-2024/Go/go/src/examples/ex30.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	amountStolen := 64650
+
+	// Checking different conditions with if, else if, else statements
+	if amountStolen > 1000000 {
+		fmt.Println("We've hit the jackpot!")
+	} else if amountStolen >= 5000 {
+		fmt.Println("Think of all the candy we can buy!")
+	} else {
+		fmt.Println("Why did we even do this?")
+	}
+}
